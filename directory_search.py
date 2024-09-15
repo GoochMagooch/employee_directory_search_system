9baff5e419e5d28f8e7cc7fb38fbb71fe754abad
Magooch <crvallecito@gmail.com>
2024-09-15 11:46:59 -0600

    Create source file and dictionary of employees

---
+++ directory_search.py
@@ -0,0 +1,1 @@
+employees = {'employee_one': ['Python', 'Discord Bots'], 'employee_two': ['Java', 'Android Dev'], 'employee_three': ['Python', 'Web Dev'], 'employee_four': ['Java', 'Android Dev'], 'employee_five': ['Python', 'Web Dev']}
