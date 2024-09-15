56afb54a3da0dc88ef0e17430484be3336fda317
Magooch <crvallecito@gmail.com>
2024-09-15 12:53:59 -0600

    Create function to add employee

--- directory_search.py
+++ directory_search.py
@@ -1,1 +1,8 @@
-employees = {'employee_one': ['Python', 'Discord Bots'], 'employee_two': ['Java', 'Android Dev'], 'employee_three': ['Python', 'Web Dev'], 'employee_four': ['Java', 'Android Dev'], 'employee_five': ['Python', 'Web Dev']}
+directory = {'employee_one': ['Python', 'Discord Bots'], 
+        'employee_two': ['Java', 'Android Dev'], 
+        'employee_three': ['Python', 'Web Dev'], 
+        'employee_four': ['Java', 'Systems Design'], 
+        'employee_five': ['Python', 'Data Analytics']}
+
+def add_employee(employee_name, lang, dept):
+    directory.append(employee_name)
